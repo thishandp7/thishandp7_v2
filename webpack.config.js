0c5c7017314f5666c4c3052a1e7217210a482d46
thishandp7 <thishandp7@gmail.com>
2018-03-11 15:30:03 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './lib/frontend/index'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: ['style-loader', 'css-loader?modules&localIdentName=[local]---[hash:base64:5]', 'cssnext-loader'] }
+    ]
+  }
+};
+
+module.exports = config;
